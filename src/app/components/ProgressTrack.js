a44fb9390451366422c5d78b1f88481139be733d
Pedro Visintin <pedrovisintin@Pedros-MacBook-Pro-2.local>
2015-05-11 20:27:49 +0200

    simplify the event handling find the DOM node early and assign to target to do it once.

--- src/app/components/ProgressTrack.js
+++ src/app/components/ProgressTrack.js
@@ -6,53 +6,65 @@ var classNames = require('classnames');
 
 var ProgressTrack = React.createClass({
   
-  getInitialState: function(){
-    return {mouseDown: false}
+  componentDidMount: function() {
+    this.target = React.findDOMNode(this);
+  },
+
+  componentWillUnmount: function() {
   },
 
   handleRelease: function(e){
-    this.handleClick(e);
-    this.setState({mouseDown: false});
-  },
 
-  handleMove: function(e){
-    if (this.state.mouseDown){
-      this.handleClick(e);
+    window.removeEventListener('mousemove', this.handleMouse);
+
+    if (e.preventDefault) {
+      e.preventDefault();
     }
+
+    window.removeEventListener('mouseup', this.handleRelease);
+
   },
 
-  handleClick: function(e){
+  handleMouse: function(e){
 
-    this.setState({mouseDown: true});
+    var barX, barWidth, x, newPosition;
 
-    var barX, barWidth, x, newPosition, target;
+    this.target = React.findDOMNode(this);
 
-    target = React.findDOMNode(this);
-
-    barX = utils.position.getOffX(target);
-    barWidth = target.offsetWidth;
+    barX = utils.position.getOffX(this.target);
+    barWidth = this.target.offsetWidth;
     x = (e.clientX - barX);
     newPosition = (x / barWidth);
 
     if (this.props.sound && this.props.sound.duration) {
-
       this.props.sound.setPosition(this.props.sound.duration * newPosition);
       // a little hackish: ensure UI updates immediately with current position, even if audio is buffering and hasn't moved there yet.
       this.props.sound._iO.whileplaying.apply(this.props.sound);
-
+    }
+    
+    if (e.preventDefault) {
+      e.preventDefault();
     }
     
   },
 
+  handleClick: function(e){
+
+    window.addEventListener('mousemove', this.handleMouse);
+    window.addEventListener('mouseup', this.handleRelease);
+
+    return this.handleMouse(e);
+
+  },
+
   render: function(){
 
-    var classes = classNames('sm2-progress-track',
-    {'dragging': this.state.mouseDown});
+    var classes = classNames('sm2-progress-track');
     
     return (
       
       /*jshint ignore:start */
-      <div className={classes} onMouseDown={this.handleClick} onMouseMove={this.handleMove} onMouseUp={this.handleRelease}>
+      <div ref='progressTrack' className={classes} onMouseDown={this.handleClick} >
         <ProgressBar  position={this.props.position} duration={this.props.duration} />
         <ProgressBall ref='progressBall' position={this.props.position} duration={this.props.duration} />
       </div>
