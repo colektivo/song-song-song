44479560004d71228939f7f5463720b65f94ac32
Pedro Visintin <pedro.visintin@gmail.com>
2015-05-30 20:57:27 +0200

    change song to inconsciente

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -3,14 +3,14 @@
 var React = require('react'),
   SongSongSongApp;
 
-var url = "https://api.soundcloud.com/tracks/73114586/stream?client_id=728189eaa03c689c2bd4eaf42f410196";
+var url = "https://api.soundcloud.com/tracks/202637761/stream?client_id=728189eaa03c689c2bd4eaf42f410196&secret_token=s-SFJ0A";
 var PlayerContainer = require('./components/PlayerContainer');
 
 SongSongSongApp = React.createClass({
   render: function() {
     return (
         /*jshint ignore:start */
-        <div>
+        <div className='demo-bd'>
             <h2>Hello, Play React Rocks.</h2>
             <PlayerContainer url={this.props.url} songName={this.props.songName} author={this.props.author} fullWidth={this.props.fullWidth}/>
         </div>
@@ -23,7 +23,7 @@ soundManager.onready(function(){
 
   var rootInstance = React.render(
       /*jshint ignore:start */
-      <SongSongSongApp url={url} songName='Morir por qué?' author='No Mataras' fullWidth='true' />,
+      <SongSongSongApp url={url} songName='Inconsciente' author='No Mataras' fullWidth='true' />,
       /*jshint ignore:end */
       document.getElementById('app')
   );
