faf3840ed0ea4fcc771ffc005e6979563e5304a8
Pedro Visintin <pedro.visintin@gmail.com>
2015-08-04 12:13:25 +0200

    add radium and style html and body

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,5 +1,6 @@
 'use strict';
-
+var Radium = require('radium');
+var Style = Radium.Style;
 var React = require('react'),
   SongSongSongApp;
 
@@ -7,10 +8,42 @@ var url = "https://api.soundcloud.com/tracks/202637761/stream?client_id=728189ea
 var PlayerContainer = require('./components/PlayerContainer');
 
 SongSongSongApp = React.createClass({
+
+  componentWillMount: function(){
+    document.body.style.backgroundImage = 'url(' + this.props.background + ')';
+  },
+  componentWillUnmount: function(){
+    document.body.style.backgroundImage = null;
+  },
   render: function() {
     return (
         /*jshint ignore:start */
-        <PlayerContainer windowed={true} url={this.props.url} songName={this.props.songName} author={this.props.author} fixed={this.props.fixed} fullWidth={this.props.fullWidth}/>
+        <div>
+          <Style rules={{
+            html: {
+              width: '100%',
+              height: '100%',
+              maxWidth: '100%',
+              maxHeight: '100%',
+              top: 0,
+              margin: 0,
+              padding: 0
+            },
+            body: {
+              width: '100%',
+              height: '100%',
+              maxWidth: '100%',
+              maxHeight: '100%',
+              top: 0,
+              margin: 0,
+              padding: 0,
+              backgroundColor : '#333',
+              backgroundSize : 'cover',
+              backgroundRepeat : 'no-repeat'
+            }
+          }} />
+          <PlayerContainer windowed={true} url={this.props.url} songName={this.props.songName} author={this.props.author} fixed={this.props.fixed} fullWidth={this.props.fullWidth}/>
+        </div>
         /*jshint ignore:end */
     );
   }
@@ -19,8 +52,9 @@ SongSongSongApp = React.createClass({
 soundManager.onready(function(){
 
   var rootInstance = React.render(
+
       /*jshint ignore:start */
-      <SongSongSongApp url={url} songName='Inconsciente' author='No Mataras' fullWidth={true} flat={true} fixed={true} />,
+      <SongSongSongApp background='/src/assets/images/inconsciente01.jpg' url={url} songName='Inconsciente' author='No Mataras' fullWidth={true} flat={true} fixed={true} />,
       /*jshint ignore:end */
       document.getElementById('app')
   );
