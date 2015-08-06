49c9a97ed17fced12a61a800de8a69984c59be68
Pedro Visintin <pedro.visintin@gmail.com>
2015-08-06 17:00:52 +0200

    migrate PlayDuration to ES6

--- src/app/components/PlayDuration.jsx
+++ src/app/components/PlayDuration.jsx
@@ -1,21 +1,35 @@
 var React = require('react');
+var Radium = require('radium');
 var utils= require('../utilities/helpers');
 var hasSound = require('./HasSoundMixin');
 
-var Duration = React.createClass({
-
-  mixins : [hasSound],
-  
-  render: function(){
+@Radium
+class Duration extends React.Component {
+  render(){
 
     var duration = utils.getTime(this.props.sound.duration, true);
 
     return (
       /*jshint ignore:start */
-      <div className="sm2-inline-duration">{duration}</div>
+      <div id="Duration" style={styles.duration}>{duration}</div>
       /*jshint ignore:end */
     );
   }
-});
+}
+
+var styles = {
+  duration: {
+    display: 'table-cell',
+    width: '1%',
+    fontSize: '75%',
+    lineHeight: '0.9em',
+    minWidth: '2em', /* if you have sounds > 10:00 in length, make this bigger. */
+    verticalAlign: 'middle'
+  }
+}
+
+Duration.propTypes = {
+  sound: React.PropTypes.object.isRequired
+};
 
 module.exports = Duration;
